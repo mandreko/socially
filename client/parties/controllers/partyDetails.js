6fcf736e69093ddb0c559228ef86e1f23815d3c8
Matt Andreko <mandreko@gmail.com>
2015-07-06 21:34:29 -0400

    Added authentication

--- client/parties/controllers/partyDetails.js
+++ client/parties/controllers/partyDetails.js
@@ -1,7 +1,8 @@
 angular.module("socially").controller("PartyDetailsCtrl",
     function($scope, $stateParams, $meteor){
 
-        $scope.party = $meteor.object(Parties, $stateParams.partyId, false);
+        $scope.party = $meteor.object(Parties, $stateParams.partyId).subscribe('parties');
+        $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
 
         $scope.save = function() {
             $scope.party.save();
